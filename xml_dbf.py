c2b3cb4d5edb48f0484592adfa082384e63a75eb
juvtorg2014 <117074157+juvtorg2014@users.noreply.github.com>
2023-07-26 16:17:40 +0300

    Done in black

--- xml_dbf.py
+++ xml_dbf.py
@@ -1,104 +1,83 @@
-from bs4 import BeautifulSoup
-import dbf as db
+import pandas as pd
+import itertools
+from bs4 import BeautifulSoup as bs
 import os
-import codecs
+import dbf
+import datetime
 
-list_firm = {(""), (""), (""), (""), (""), (""), (""), ("")}
+def xml_to_csv(name_file):
+    with open(name_file, 'r', encoding='UTF-8') as f:
+        content = f.read()
 
-def create_dbf(date_file):
-    new_table = db.Table(date_file + ".dbf",
-            'NUMBER C(8); KODCR C(5); KODCN C(5); SD C(20); RG C(20); NAMEU C(200); ADRESS C(200); GR D',
-            codepage='cp866')
-    new_table.open(db.READ_WRITE)
-    try:
-        for n, item in enumerate(list_firm):
-            if len(item) > 0:
-                new_table.append(item)
-                print(n, item[5])
-    except IndexError:
-        print(Exception)
-    finally:
-        new_table.close()
+        # Извлечение содержания файла по спискам
+        soup = bs(content, "xml")
+        date_list = soup.find("ДатаПеречня").text.replace("-", "")
+        name_csv = date_list + '.csv'
+        full_name_csv = os.path.abspath(name_csv)
+        ps_name = [values.text for values in soup.findAll('ФИО')]
+        ps_date = [values.text.replace('-','.') for values in soup.findAll('ДатаРождения')]
+        ps_seria = [values.text for values in soup.findAll('Серия')]
+        ps_number = [values.text for values in soup.findAll('Номер')]
+        ps_adress = [values.text for values in soup.findAll('ТекстАдреса')]
+        ps_cr = [values.text for values in soup.findAll('Код')]
+        ps_cn = [values.text for values in soup.findAll('Код')]
 
+        # Собираем все списки в общую таблицу
+        data = [item for item in itertools.zip_longest(ps_cr, ps_cn, ps_seria, ps_number, ps_name, ps_date, ps_adress)]
+        df = pd.DataFrame(data=data)
+        df.columns = ['KODCR', 'KODCN', 'SD', 'RG', 'NAMEU', 'GR', 'ADRESS']
 
-def xml_dbf(name):
-    with open(name, 'r', encoding='utf-8') as fr:
-        xml_file = fr.read()
-        soup = BeautifulSoup(xml_file, 'xml')
-        date_file = soup.find("ДатаПеречня").text.replace("-", "")
-        nodes = soup.find('АктуальныйПеречень')
-        subjects = nodes.find_all('Субъект')
-        for n, node in enumerate(subjects):
-            if node.find("ФЛ"):
-                if node.find("ФИО"):
-                    name = node.find("ФИО").text
-                    names = name[:100] if len(name) > 100 else name
-                    #names = name.encode().decode('cp866', errors='ignore')
+        # Изменение даты в формат dd.mm.YY
+        df['GR'] = pd.to_datetime(df['GR'], dayfirst=False)
+        df['GR'] = df['GR'].dt.strftime('%d.%m.%Y')
+
+        # Сохранение в файл
+        if os.path.exists(full_name_csv):
+            os.remove(full_name_csv)
+        df.to_csv(full_name_csv, index=False, header=True, encoding='cp1251')
+    return full_name_csv
+
+def csv_to_dbf(name_file):
+    dbf_file = name_file.replace('.csv', '.dbf')
+    if os.path.exists(dbf_file):
+        os.remove(dbf_file)
+
+    table = dbf.Table(dbf_file,
+                      'KODCR C(14); KODCN C(14); SD C(50); RG C(50); NAMEU C(250); GR D; ADRESS C(250)',
+                      codepage='cp866')
+    table.open(dbf.READ_WRITE)
+
+    with open(name_file, 'r', encoding='cp1251') as f:
+        while f.readline():
+            new_line = []
+            line = f.readline().split(',')
+            if len(line) > 6:
+                new_line.append(line[0]) # KODCR
+                new_line.append(line[1]) # KODCN
+                new_line.append(line[2]) # SD
+                new_line.append(line[3]) # RG
+                new_line.append(line[4].rstrip())
+
+                if len(line[5]):
+                    dd, mm, yy = line[5].split('.')
+                    date_birth = datetime.datetime(int(yy), int(mm), int(dd))
                 else:
-                    name = ' '
-                if node.find("ДатаРождения"):
-                    date_birth = node.find("ДатаРождения").text
-                    birth = db.Date(int(date_birth[:4]) , int(date_birth[5:7]), int(date_birth[8:]))
-                else:
-                    birth = db.Date(int('2000'), int("01"), int("01"))
-                if node.find("Серия"):
-                    series = node.find("Серия").text
-                else:
-                    series = ' '
-                if node.find("Номер"):
-                    number = node.find("Номер").text
-                else:
-                    number = ' '
-                if node.find("ТекстАдреса"):
-                    adres = node.find("ТекстАдреса").text
-                    adress = adres[:100] if len(adres) > 100 else adres
-                    #adress = adres.encode().decode('cp866', errors='ignore')
-                else:
-                    adress = " "
-                if node.find("Код"):
-                    code = node.find("Код").text
-                else:
-                     if node.find("Гражданство"):
-                         if node.find("Гражданство").text == "РОССИЙСКАЯ ФЕДЕРАЦИЯ":
-                             code = "843"
-                     else:
-                         code = ' '
-                tuple_line = (str(n+1), code, code, series, number, names, adress, birth)
-                print(tuple_line)
-                list_firm.add(tuple_line)
-            elif node.find("Орг"):
-                if node.find("Орг").find("Наименование"):
-                    name = node.find("Орг").find("Наименование").text
-                    name = name[:100] if len(name) > 100 else name
-                    names = name.encode().decode("cp866", errors='ignore')
-                else:
-                    name = ' '
-                if node.find("Орг").find('ИНН'):
-                    series = node.find("Орг").find('ИНН').text
-                else:
-                    series = ' '
-                if node.find("Орг").find('ОГРН'):
-                    number = node.find("Орг").find('ОГРН').text
-                else:
-                    number = ' '
-                if node.find("ТекстАдреса"):
-                    adres = node.find("ТекстАдреса").text
-                    adres = adres[:100] if len(adres) > 100 else adres
-                    adress = adres.encode().decode("cp866", errors='ignore')
-                else:
-                    adress = ' '
-                if node.find("Страна"):
-                    code = node.find("Страна").find("Код").text
-                else:
-                    code = ' '
-                #tuple_line = (str(n + 1), code, code, series, number, names, adress)
-                #list_firm.add(tuple_line)
-    if os.path.exists(date_file):
-        os.remove(date_file)
-        create_dbf(date_file)
-    else:
-        create_dbf(date_file)
+                    date_birth= datetime.datetime(2000,1,1)
+                new_line.append(date_birth)
+
+                new_line.append(''.join(line[6:len(line)-1]).rstrip())
+                table.append(tuple(new_line))
+                print(date_birth)
+            else:
+                continue
+    table.close()
+    print(f"Создан файл {dbf_file}")
 
 
 if __name__ == '__main__':
-    xml_dbf('25.04.2023.xml')
+    #name_file = input('Введите имя файла без <xml>\n')
+    # if not os.path.exists(os.path.abspath(name_file)):
+    #     "Неправильное имя файла !!!"
+    #     exit()
+    name_file = xml_to_csv('20.07.2023.xml')
+    csv_to_dbf(name_file)
